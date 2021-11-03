960c3ac4cf2d8453aebe7776f60122a861156e28
paulos-t <65367687+paulos-t@users.noreply.github.com>
2021-11-03 02:36:45 -0400

    Update and rename 10001st-prime-inefficient.py to 10001st-prime.py

--- 10001st-prime.py
+++ 10001st-prime.py
@@ -1,6 +1,6 @@
 
-def prime_check(x):
-    for i in range(2, x):
+def prime_check(x, out):
+    for i in out:
         if x % i == 0:
             return False
     return True
@@ -8,7 +8,7 @@ def prime_check(x):
 i = 2
 out = []
 while len(out) < 10001:
-    if prime_check(i):
+    if prime_check(i, out):
         out.append(i)
     i += 1
 print(out[-1])
